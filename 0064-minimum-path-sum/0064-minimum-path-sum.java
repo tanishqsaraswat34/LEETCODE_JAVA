725dc5b91d1e2820608337d70fdc7b3af1f0a1ef
tanishqsaraswat34 <151999977+tanishqsaraswat34@users.noreply.github.com>
2026-09-07 06:51:07 +0530

    Time: 6 ms (9.91%), Space: 50.4 MB (38.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int[][] dp= new int[n][m];
-        for (int[] row : dp ){
-            Arrays.fill(row,-1);
+        int n =grid.length;
+        int m =grid[0].length;
+        int[][] dp=new int[n][m];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                }
+                else{
+                    int up=grid[i][j];
+                    if (i>0) up+=dp[i-1][j];
+                    else up+=1e9;
+
+                    int left=grid[i][j];
+                    if (j>0) left+=dp[i][j-1];
+                    else left+=1e9;
+
+                    dp[i][j]=Math.min(up,left);
+                }
+            }
+            
         }
-        return calminPath(n-1,m-1,grid,dp);
+        return dp[n-1][m-1];
         
-    }
-    public int calminPath(int i, int j, int[][] grid, int[][] dp){
-        if (i==0 && j==0){
-            return grid[0][0];
-        }
-        if(i<0 || j<0){
-            return (int)1e9;
-
-        }
-
-        if (dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        int up=grid[i][j]+  calminPath(i-1,j,grid,dp);
-        int down=grid[i][j]+calminPath(i,j-1,grid,dp);
-        dp[i][j]=Math.min(up,down);
-        return dp[i][j];
     }
 }
